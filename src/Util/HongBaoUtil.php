17eb6d4996aee6e4d60fba0c9596419ce520ae93
l25359423 <425350428@qq.com>
2022-06-19 02:39:08 +0800

    update

--- src/Util/HongBaoUtil.php
+++ src/Util/HongBaoUtil.php
@@ -16,6 +16,6 @@ class HongBaoUtil
         $config = app('flarum.config');
         $root_url = (string)$config->url();
         $waimai_url = $root_url . "/waimai";
-        return sprintf("点击下方链接领取外卖红包，开启干饭模式~\n%s", $waimai_url);
+        return sprintf("点击下方链接领取美团饿了么外卖红包，最高可领66元，快去领取红包开启干饭模式吧[坏笑]~\n\n%s", $waimai_url);
     }
 }
