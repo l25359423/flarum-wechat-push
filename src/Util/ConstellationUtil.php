15ac6d732b0ee680be4fb5b918c29912c74da59b
l25359423 <425350428@qq.com>
2022-06-19 16:22:52 +0800

    update

--- src/Util/ConstellationUtil.php
+++ src/Util/ConstellationUtil.php
@@ -4,18 +4,54 @@ namespace Leo\WechatPush\Util;
 class ConstellationUtil
 {
     protected static $constellations = array(
-        "白羊座" => "aries",
-        "金牛座" => "taurus",
-        "双子座" => "gemini",
-        "巨蟹座" => "cancer",
-        "狮子座" => "leo",
-        "处女座" => "virgo",
-        "天秤座" => "libra",
-        "天蝎座" => "scorpio",
-        "射手座" => "sagittarius",
-        "摩羯座" => "capricorn",
-        "水瓶座" => "aquarius",
-        "双鱼座" => "pisces",
+        "白羊座" => array(
+            "file" => "aries",
+            "symbol" => "♈"
+        ),
+        "金牛座" => array(
+            "file" => "taurus",
+            "symbol" => "♉"
+        ),
+        "双子座" => array(
+            "file" => "gemini",
+            "symbol" => "♊"
+        ),
+        "巨蟹座" => array(
+            "file" => "cancer",
+            "symbol" => "♋"
+        ),
+        "狮子座" => array(
+            "file" => "leo",
+            "symbol" => "♌"
+        ),
+        "处女座" => array(
+            "file" => "virgo",
+            "symbol" => "♍"
+        ),
+        "天秤座" => array(
+            "file" => "libra",
+            "symbol" => "♎"
+        ),
+        "天蝎座" => array(
+            "file" => "scorpio",
+            "symbol" => "♏"
+        ),
+        "射手座" => array(
+            "file" => "sagittarius",
+            "symbol" => "♐"
+        ),
+        "摩羯座" => array(
+            "file" => "capricorn",
+            "symbol" => "♑"
+        ),
+        "水瓶座" => array(
+            "file" => "aquarius",
+            "symbol" => "♒"
+        ),
+        "双鱼座" => array(
+            "file" => "pisces",
+            "symbol" => "♓"
+        ),
     );
 
     public static function check($content)
@@ -26,13 +62,13 @@ class ConstellationUtil
     {
         $reply_content = "";
         $json = file_get_contents(base_path() .
-            sprintf("/constellation/%s.json", self::$constellations[$content]));
+            sprintf("/constellation/%s.json", self::$constellations[$content]['file']));
         if($json){
             $arr = json_decode($json, true);
             $reply_content .= sprintf("💗%s💗\n\n", $content);
             foreach ($arr as $key => $val){
-                $reply_content .= sprintf("♊%s: %s%s\n\n",
-                    $key, $val['star'],
+                $reply_content .= sprintf("%s%s: %s%s\n\n",
+                    self::$constellations[$content]['symbol'], $key, $val['star'],
                     ($val['text'] ? "\n" . $val['text'] : ""));
             }
         }
